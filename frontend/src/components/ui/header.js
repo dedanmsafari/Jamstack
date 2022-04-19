ba7cd8101e96fbc0a80fd5ef5da5aedb61fe5914
dedanmsafari <dedan.msafari@gmail.com>
2022-04-19 10:30:49 +0300

    added routing to the header

--- frontend/src/components/ui/header.js
+++ frontend/src/components/ui/header.js
@@ -14,6 +14,8 @@ import { Hidden } from "@material-ui/core"
 import { useMediaQuery } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 
+import { Link, navigate } from "gatsby"
+
 import search from "../../images/search.svg"
 import account from "../../images/account-header.svg"
 import cart from "../../images/cart.svg"
@@ -23,16 +25,32 @@ const useStyles = makeStyles(theme => ({
   coloredIndicator: {
     backgroundColor: theme.palette.common.white,
   },
+  drawer: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  listItemText: {
+    color: theme.palette.common.white,
+  },
   logoText: {
     color: theme.palette.common.offBlack,
+  },
+  logoContainer: {
+    [theme.breakpoints.down("md")]: {
+      marginRight: "auto",
+    },
+  },
+  tab: {
+    ...theme.typography.body1,
+    fontWeight: 600,
+    fontSize: "1rem",
   },
   tabs: {
     marginLeft: "auto",
     marginRight: "auto",
   },
   imgIcon: {
-    width: "3rem",
-    height: "3rem",
+    width: "1.5rem",
+    height: "1.5rem",
   },
 }))
 
@@ -44,7 +62,25 @@ export default function Header({ categories }) {
 
   const routes = [
     ...categories,
-    { node: { name: "Contact Us", strapiId: `1234${Math.random()}` } },
+    {
+      node: {
+        name: "Contact Us",
+        strapiId: `1234${Math.random()}`, //Geek way of creating random number lol!
+        link: "/contact",
+      },
+    },
+  ]
+
+  const actions = [
+    { icon: search, alt: "search", visible: true },
+    { icon: cart, alt: "cart", visible: true, link: "/cart" },
+    { icon: account, alt: "account", visible: !matchesMD, link: "/account" },
+    {
+      icon: menu,
+      alt: "menu",
+      visible: matchesMD,
+      onClick: () => setDrawerOpen(true),
+    },
   ]
 
   const iOS =
@@ -57,7 +93,15 @@ export default function Header({ categories }) {
       classes={{ indicator: classes.coloredIndicator, root: classes.tabs }}
     >
       {routes.map(r => {
-        return <Tab label={r.node.name} key={r.node.strapiId} />
+        return (
+          <Tab
+            component={Link}
+            to={r.node.link || `/${r.node.name.toLowerCase()}`}
+            classes={{ root: classes.tab }}
+            label={r.node.name}
+            key={r.node.strapiId}
+          />
+        )
       })}
     </Tabs>
   )
@@ -68,11 +112,15 @@ export default function Header({ categories }) {
       onClose={() => setDrawerOpen(false)}
       disableBackdropTransition={!iOS}
       disableDiscovery={iOS}
+      classes={{ paper: classes.drawer }}
     >
       <List disablePadding>
         {routes.map(r => (
           <ListItem divider button key={r.node.strapiId}>
-            <ListItemText primary={r.node.name} />
+            <ListItemText
+              classes={{ primary: classes.listItemText }}
+              primary={r.node.name}
+            />
           </ListItem>
         ))}
       </List>
@@ -82,23 +130,26 @@ export default function Header({ categories }) {
   return (
     <AppBar color="transparent" elevation={0}>
       <Toolbar>
-        <Typography variant="h1">
-          <span className={classes.logoText}>VAR </span> X
-        </Typography>
+        <Button classes={{ root: classes.logoContainer }}>
+          <Typography variant="h1">
+            <span className={classes.logoText}>VAR </span> X
+          </Typography>
+        </Button>
         {matchesMD ? drawer : tabs}
-        <IconButton>
-          <img className={classes.imgIcon} src={search} alt="search" />
-        </IconButton>
-        <IconButton>
-          <img className={classes.imgIcon} src={cart} alt="cart" />
-        </IconButton>
-        <IconButton onClick={() => (matchesMD ? setDrawerOpen(true) : null)}>
-          <img
-            className={classes.imgIcon}
-            src={matchesMD ? menu : account}
-            alt={matchesMD ? "menu" : "account"}
-          />
-        </IconButton>
+        {actions.map(action => {
+          if (action.visible) {
+            return (
+              <IconButton key={action.alt} component={Link} to={action.link}>
+                <img
+                  onClick={action.onClick}
+                  className={classes.imgIcon}
+                  src={action.icon}
+                  alt={action.alt}
+                />
+              </IconButton>
+            )
+          }
+        })}
       </Toolbar>
     </AppBar>
   )
