69c046e6c91b24f6db492bfe2863e7b8b4e2bd8f
dedanmsafari <dedan.msafari@gmail.com>
2022-04-19 12:31:28 +0300

    created the shirts page

---
+++ frontend/src/pages/shirts.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/ui/layout"
+
+const Shirts = () => (
+  <Layout>
+    <p>Our Shirts</p>
+  </Layout>
+)
+
+export default Shirts
