ba43924205758e13b98f6581510344d3dad384c6
dedanmsafari <dedan.msafari@gmail.com>
2022-04-19 12:30:44 +0300

    initialised the contact us page

---
+++ frontend/src/pages/contact.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/ui/layout"
+
+const Contact = () => (
+  <Layout>
+    <p>Contact us</p>
+  </Layout>
+)
+
+export default Contact
