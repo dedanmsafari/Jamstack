6d36249ef2d5b21f4c170b11689c81f1deed9229
dedanmsafari <dedan.msafari@gmail.com>
2022-04-26 22:38:02 +0300

    added marketing and call to action

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -5,12 +5,15 @@ import Layout from "../components/ui/layout"
 import HeroBlock from "../components/Home/heroblock"
 import PromotionalBlock from "../components/Home/promotionalblock"
 import FeaturedProducts from "../components/Home/featuredProducts"
-
+import MarketingButtons from "../components/Home/MarketingButtons"
+import CallToAction from "../components/Home/callToAction"
 const IndexPage = () => (
   <Layout>
     <HeroBlock />
     <PromotionalBlock />
     <FeaturedProducts />
+    <MarketingButtons />
+    <CallToAction />
   </Layout>
 )
 
