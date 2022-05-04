7c7bc6e83eefdf67f9e627762ae362ddbef7508b
dedanmsafari <dedan.msafari@gmail.com>
2022-05-04 18:55:15 +0300

    added our page queries to list

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -1,9 +1,54 @@
-exports.createPages = async ({ actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+
+  const results = await graphql(`
+    {
+      products: allStrapiProduct {
+        edges {
+          node {
+            name
+            strapiId
+          }
+        }
+      }
+      categories: allStrapiCategory {
+        edges {
+          node {
+            strapiId
+            name
+            description
+          }
+        }
+      }
+    }
+  `)
+  if (results.errors) {
+    throw results.errors
+  }
+
+  const products = results.data.products.edges
+  const categories = results.data.categories.edges
+
+  products.forEach(product => {
+    createPage({
+      path: `/products/${product.node.strapiId}`,
+      component: path.resolve(`./src/templates/product.js`),
+      context: {
+        id: product.node.strapiId,
+        name: product.node.name,
+      },
+    })
+  })
+
+  categories.forEach(category => {
+    createPage({
+      path: `/${category.node.name.toLowerCase()}`,
+      component: path.resolve(`./src/templates/category.js`),
+      context: {
+        id: category.node.strapiId,
+        name: category.node.name,
+        description: category.node.description,
+      },
+    })
   })
 }
